ff7b8fbedc764a30e5b455694e8fc45fba24e36c
华成伍 <chengwu.hua@mtime.com>
2018-07-13 13:28:38 +0800

    添加模块

---
+++ SimpleRPCEtcdRegisterCenter/src/main/java/org/net/sea/simple/rpc/register/center/etcd/App.java
@@ -0,0 +1,13 @@
+package org.net.sea.simple.rpc.register.center.etcd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SimpleRPCRegisterCenter/src/main/java/net/sea/simple/rpc/register/center/App.java
@@ -0,0 +1,13 @@
+package net.sea.simple.rpc.register.center;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
